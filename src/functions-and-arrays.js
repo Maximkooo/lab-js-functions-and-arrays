3f94c60c95e728ec7964c672e92c9c9715602233
Maksym <citomax7196@gmail.com>
2024-01-10 22:52:34 +0100

    solve all js tasks

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,19 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  return num1 > num2 ? num1 : num2
+}
 
-
+console.log(maxOfTwoNumbers(4, 7));
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  return arr.sort((a, b) => b.length - a.length)[0]
+}
+
+console.log(findLongestWord(words));
 
 
 
@@ -15,7 +21,11 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers() {
+  return numbers.reduce((acc, curr) => acc + curr, 0)
+}
+console.log(sumNumbers(numbers));
+
 
 
 
@@ -23,12 +33,20 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(arr) {
+  return sumNumbers(arr) / arr.length
+}
+console.log(averageNumbers(numbers2));
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr, val) {
+  if (!arr.length) return null
+  return arr.includes(val)
+}
+
+
+console.log(doesWordExist([], 'trut'));
